bbcb01c703e559331700959f1eb01d1fb4ec60f0
steft <52441940+stef-t@users.noreply.github.com>
2021-07-22 11:39:32 +0200

    disabling default test case

--- spec/unity_export_spec.rb
+++ spec/unity_export_spec.rb
@@ -1,9 +1,9 @@
 describe Fastlane::Actions::UnityExportAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The unity_exporter plugin is working!")
-
-      Fastlane::Actions::UnityExportAction.run(nil)
-    end
+    #it 'prints a message' do
+    #  expect(Fastlane::UI).to receive(:message).with("The unity_exporter plugin is working!")
+    #
+    #  Fastlane::Actions::UnityExportAction.run(nil)
+    #end
   end
 end
